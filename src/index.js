36d8be1d2bf28694dc0dca7ea45ad04db4dbcff1
Y Mani Chandra Reddy <manichandra842@gmail.com>
2026-05-14 20:49:59 +0530

    Added Arcjet security features

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import http from 'http';
 import { matchRouter } from "./routes/matches.js";
 import 'dotenv/config';
 import { attachWebSocketServer } from "./ws/server.js";
+import { securityMiddleware } from "./routes/arcjet.js";
 
 const PORT = Number(process.env.PORT) || 8000;
 const HOST = process.env.HOST || '0.0.0.0';
@@ -17,6 +18,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello from Express" });
 });
 
+app.use(securityMiddleware());
 app.use('/matches',matchRouter);
 
 const {broadcastMatchCreated} = attachWebSocketServer(server);
--- src/ws/server.js
+++ src/ws/server.js
@@ -1,4 +1,5 @@
 import { WebSocket, WebSocketServer } from "ws";
+import { wsArcjet } from "../routes/arcjet.js";
 
 const HEARTBEAT_INTERVAL_MS = 30000;
 
@@ -37,7 +38,28 @@ export function attachWebSocketServer(server){
         clearInterval(interval);
     });
     
-    wss.on('connection',(socket) => {
+    wss.on('connection',async (socket,req) => {
+
+        if(wsArcjet){
+            try{
+                const decision = await wsArcjet.protect(req);
+
+                if(decision.isDenied()){
+                    const code = decision.reason.isRateLimit() ? 1013 : 1008;
+                    const reason = decision.reason.isRateLimit() ? 'Rate limit exceeded' : 'Access denied';
+
+                    socket.close(code,reason);
+                    // if(decision.reason.isRateLimit()){
+                    //     // return resizeBy.status(423).json({'error':'Too many requests'});
+                    // }
+                }
+            }catch(e){
+                console.error('WS Connection Error:',e);
+                socket.close(1011,'Server Security Error');
+                return;
+            }
+        }
+
         socket.isAlive = true;
         sendJSON(socket, {type:'welcome'});
 
